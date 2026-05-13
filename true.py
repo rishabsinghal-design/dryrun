bd13e5ac5c351fa654dc4b0739557d37f6fb2256
rishabsinghal-design <rishab.singhal@aavya.com>
2026-05-13 13:52:10 +0530

    deprecate: Add deprecation notice to true.py

--- true.py
+++ true.py
@@ -1,2 +1,6 @@
+# DEPRECATED: This file is deprecated and will be removed in Phase 3.
+# The equivalent functionality has been rewritten in JavaScript in true.js.
+# Please use true.js instead.
+
 def hello_world():
     return "Hello, World!"
